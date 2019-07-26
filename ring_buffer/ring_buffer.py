241d603f9e54337b09b2cf9bf68173b8b308580c
Sem8 <mssemmi8@gmail.com>
2019-07-26 12:00:22 -0400

    got the ring buffer get method working still cannot get append method to completely pass tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,26 @@
 class RingBuffer:
-  def __init__(self, capacity):
+  def __init__(self, capacity=0):
     self.capacity = capacity
     self.current = 0
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    if self.current <= self.capacity - 1:
+      self.storage[self.current] = item
+      self.current += 1
+    if self.current == self.capacity:
+      self.current = 0
+      self.storage[self.current] = item
+    
+    
 
   def get(self):
-    pass
+    return [element for element in self.storage if element != None]
+    
+    
+
+
+
+# self.storage is array itself
+# self.current is the index
+# self.capacity is size of array or len of self.storage
